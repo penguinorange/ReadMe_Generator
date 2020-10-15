5ead35bed576760e58bedf22bfaedcfe8032ae88
jellyfish <johnrclifford@gmail.com>
2020-10-14 20:45:14 -0500

    finished code, generated first readme from the application

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# ReadMe_Generator
-Command line application for generating readMe files
+
+  # ReadMe_Generator
+  [![License](https://img.shields.io/badge/License-MIT-blue.svg)]
+
+  ## Description
+  A fast and simple way to generate a ReadMe
+  
+  ## Table of Contents
+  
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+
+## Installation
+npm i
+
+## Usage
+To expedite the readme process
+
+## License
+This project is covered by the MIT license.
+
+## Contributing
+You can't
+
+## Tests
+npm test
+
+## Questions
+Check me out on Github: [penguinorange](https://github.com/penguinorange)
+
+Contact me via email: johnrclifford@gmail.com
+
+
+  
---
+++ index.js
@@ -0,0 +1,125 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?"
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?"
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Briefly describe your project"
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Please provide installation instructions for dependencies",
+      default: "npm i"
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What is the purpose of this project"
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What license are you using?",
+      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Explain how people may contribute to the repository"
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Which commands can be run for testing this project?",
+      default: "npm test"
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "Is there anything the user should know regarding the use of this repository (if no, press enter)?"
+    },
+  ]);
+}
+
+function writeFile(answers) {
+  return `
+  # ${answers.title}
+  [![License](https://img.shields.io/badge/License-${answers.license}-blue.svg)]
+
+  ## Description
+  ${answers.description}
+  
+  ## Table of Contents
+  
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is covered by the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+Check me out on Github: [${answers.github}](https://github.com/${answers.github})
+
+Contact me via email: ${answers.email}
+
+${answers.questions}
+  `;
+}
+
+promptUser()
+  .then(function (answers) {
+    const NewMarkdown = writeFile(answers);
+
+    return writeFileAsync("README.md", NewMarkdown);
+  })
+  .then(function () {
+    console.log("Successfully wrote to README.md");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+
